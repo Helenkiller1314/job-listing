1c0fb4eb95b803465a77d7007097e06540cb3a14
Helen <helenmayanna@gmail.com>
2017-05-03 17:50:36 +0800

    add city company and category to job

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -79,6 +79,6 @@ before_action :validate_search_key, only: [:search]
   private
 
   def job_params
-    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden, :city, :company, :category)
   end
 end
